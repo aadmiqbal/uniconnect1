3fe63a012c14e1b8ba04532d78d8d2dded54165f
xxk150 <xxk150>
2023-03-07 13:50:07 +0000

    added createprofile folder

---
+++ src/main/webapp/app/createProfile/createprofile.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-createprofile',
+  templateUrl: './createprofile.component.html',
+  styleUrls: ['./createprofile.component.scss'],
+})
+export class CreateprofileComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
