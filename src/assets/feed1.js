49d3250c271ee2793902086246fa87e6f9714f37
Krishaad <123177833+Krishaad@users.noreply.github.com>
2023-04-17 11:39:41 +0100

    added fronteend bit for connections list

--- src/assets/feed1.js
+++ src/assets/feed1.js
@@ -231,9 +231,35 @@ function populatePopup(profilePopup, user) {
   connectButton.style.position = 'absolute';
   connectButton.style.bottom = '10px'; //TODO: done 'cheating'
 
-  const connect = function () {
+  /*const connect = function () {
     alert("'Connect' pushed!");
   }; //TODO: call backend to add connection
+  connectButton.addEventListener('click', connect);
+*/
+
+  const connect = function () {
+    const userId1 = document.getElementById('userId1').value; // Get the first user ID from the input field
+    const userId2 = document.getElementById('userId2').value; // Get the second user ID from the input field
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ userId1: userId1, userId2: userId2 }),
+    };
+
+    fetch('http://localhost:8080/api/connections', requestOptions)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to add connection');
+        }
+        alert('Connection added successfully!');
+      })
+      .catch(error => {
+        console.error(error);
+        alert('Failed to add connection');
+      });
+  };
+
   connectButton.addEventListener('click', connect);
 
   brl.appendChild(modulesField);
@@ -286,4 +312,144 @@ function addModuleLabel() {
   moduleLabels.appendChild(br1);
   moduleLabels.appendChild(removeBtn);
   moduleLabels.appendChild(br2);
+}
+
+// Define arrays
+let friends = [
+  { name: 'Roshaan', image: imagesrc, status: 'Online' },
+  { name: 'Adam', image: imagesrc, status: 'Offline' },
+  { name: 'Axel', image: imagesrc, status: 'Away' },
+];
+let requests = [
+  { name: 'Krishaad', image: imagesrc },
+  { name: 'Varan', image: imagesrc },
+  { name: 'Einstein', image: imagesrc },
+  { name: 'Robin', image: imagesrc },
+];
+
+// Function to display friends list
+function displayFriends() {
+  // Get the section element where we want to display the cards
+  let section = document.getElementById('list');
+
+  // Clear any previous content
+  section.innerHTML = '';
+
+  // Loop through the friends array and create a card for each friend
+  for (let i = 0; i < friends.length; i++) {
+    // Create the card container
+    let card = document.createElement('div');
+    card.classList.add('card', 'mb-3');
+
+    // Create the card body
+    let cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+
+    // Create the card title with the friend's name
+    let cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = friends[i].name;
+
+    // Create the card text with the friend's status
+    let cardText = document.createElement('p');
+    cardText.classList.add('card-text');
+    cardText.textContent = 'Status: ' + friends[i].status;
+
+    // Create the profile picture and add it to the card
+    let profilePic = document.createElement('img');
+    profilePic.classList.add('rounded', 'float-end');
+    profilePic.style.width = '75px';
+    profilePic.style.height = '75px';
+    profilePic.style.objectFit = 'cover';
+    profilePic.src = friends[i].image;
+    cardBody.appendChild(profilePic);
+
+    let chatButton = document.createElement('button');
+    chatButton.textContent = 'Chat';
+
+    // Add the title and text to the card body
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardText);
+
+    // Add the card body to the card container
+    card.appendChild(cardBody);
+    card.appendChild(chatButton);
+
+    // Add the card to the section
+    section.appendChild(card);
+  }
+}
+
+// Function to display requests list
+function displayRequests() {
+  // Get the section element where we want to display the cards
+  let section = document.getElementById('list');
+
+  // Clear any previous content
+  section.innerHTML = '';
+
+  // Loop through the requests array and create a card for each request
+  for (let i = 0; i < requests.length; i++) {
+    // Create the card container
+    let card = document.createElement('div');
+    card.classList.add('card', 'mb-3', 'justify-content-between', 'd-flex');
+    card.style.maxwidth = 'max-content';
+    card.style.maxheight = 'max-content';
+
+    // Create the card body
+    let cardBody = document.createElement('div');
+    cardBody.classList.add('card-body', 'd-flex', 'justify-content-between', 'align-items-center');
+
+    // Create the profile picture and add it to the card
+    let profilePic = document.createElement('img');
+    profilePic.classList.add('rounded', 'float-right', 'me-1');
+    profilePic.style.width = '75px';
+    profilePic.style.height = '70px';
+    // profilePic.style.objectFit = 'center-center';
+    profilePic.src = requests[i].image;
+
+    // Create the card title with the friend's name
+    let cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title', 'me-1', 'justify-content-center');
+    cardTitle.textContent = requests[i].name;
+
+    // Create the accept button
+    let acceptButton = document.createElement('button');
+    acceptButton.type = 'button';
+    acceptButton.classList.add('btn', 'btn-success', 'btn-sm', 'float');
+    acceptButton.textContent = 'Accept';
+    acceptButton.style.maxwidth = 'max-content';
+    acceptButton.style.maxheight = 'max-content';
+
+    acceptButton.addEventListener('click', function () {
+      // Handle accept button click
+    });
+
+    // Create the reject button
+    let rejectButton = document.createElement('button');
+    rejectButton.type = 'button';
+    rejectButton.classList.add('btn', 'btn-danger', 'btn-sm', 'float');
+    rejectButton.textContent = 'Decline';
+    rejectButton.style.maxwidth = 'max-content';
+    rejectButton.style.maxheight = 'max-content';
+
+    rejectButton.addEventListener('click', function () {
+      // Handle reject button click
+    });
+
+    cardBody.appendChild(profilePic);
+    cardBody.appendChild(cardTitle);
+
+    // buttonBody.appendChild(acceptButton);
+    // buttonBody.appendChild(rejectButton);
+    // cardBody.appendChild(buttonBody);
+
+    // Add the card body to the card container
+    card.appendChild(cardBody);
+    card.appendChild(acceptButton);
+    card.appendChild(rejectButton);
+
+    // Add the card to the section
+    section.appendChild(card);
+  }
 }
