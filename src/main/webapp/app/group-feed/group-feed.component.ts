9d7bf63a780aef936a2a4dbd57a7cf8c7922f051
Axel Yalmaz <axy125@student.bham.ac.uk>
2023-04-28 21:12:23 +0100

    create a group backend(no pic, admins)

--- src/main/webapp/app/group-feed/group-feed.component.ts
+++ src/main/webapp/app/group-feed/group-feed.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 declare function greet1(finalUsers: any[]): void;
 
@@ -9,12 +10,32 @@ declare function greet1(finalUsers: any[]): void;
   styleUrls: ['./group-feed.component.scss'],
 })
 export class GroupFeedComponent implements OnInit {
-  /* myScriptElement: HTMLScriptElement; */
-  constructor(private http: HttpClient) {}
+  groupForm!: FormGroup;
+  constructor(private http: HttpClient, private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.http.get<any[]>('/api/final-users').subscribe(finalUsers => {
-      greet1(finalUsers);
+    this.groupForm = this.fb.group({
+      name: ['', Validators.required],
+      groupDescription: ['', Validators.required],
+      groupAvatar: [''],
     });
+  }
+  onSubmit(): void {
+    if (this.groupForm.valid) {
+      const formData = this.groupForm.value;
+      this.http.post('/api/final-groups', formData).subscribe(
+        result => {
+          console.log('Group created:', result);
+          // Show success message and close popup here
+          alert('Group created successfully');
+          location.reload();
+        },
+        error => {
+          console.error('Error creating group:', error);
+          // Show error message here
+          alert('Error creating group');
+        }
+      );
+    }
   }
 }
